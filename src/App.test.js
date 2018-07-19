7139112e92dce896b0f81ec195a02eae9cd4f450
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-19 09:49:59 +0530

    Step 1: Install Enzyme and Right Test for Component

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,8 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  // ReactDOM should only be used for functionality, not for testing purposes.
+  // Use enzyme instead
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(ReactDOM.unmountComponentAtNode(div)).toEqual(true);
 });
