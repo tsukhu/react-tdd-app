1eeed431d3ea041788f8f5ad8ced0d3528654b15
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-19 09:59:23 +0530

    Added Snapshot test

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Button from './Button';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 it('should render without crashing', () => {
   shallow(<Button />);
+});
+
+it('should match the snapshot', () => {
+  const component = renderer.create(<Button />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
