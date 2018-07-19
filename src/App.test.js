b29cfd3484a694d1837508c14f36ec334092074f
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-07-19 10:14:27 +0530

    Add integration test with mount

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -8,4 +9,10 @@ it('renders without crashing', () => {
   // Use enzyme instead
   ReactDOM.render(<App />, div);
   expect(ReactDOM.unmountComponentAtNode(div)).toEqual(true);
+});
+
+it('should render the button correctly', () => {
+  const wrapper = mount(<App />);
+  const buttonText = 'Press';
+  expect(wrapper.contains(buttonText)).toEqual(true);
 });
